cc9bad187ed14929d50a1e620396f7ab0be985f5
swaraj singh <swarajsingh494@gmail.com>
2024-12-09 20:50:15 +0530

    Successfully made the website

--- script.js
+++ script.js
@@ -4,27 +4,434 @@ const lenis = new Lenis({
 });
 
 // Listen for the scroll event and log the event data
-
-var frames = { currentIndex: 0, maxIndex: 1879 };
+var frames = { currentIndex: 0, maxIndex: 0 };
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
 var Images = [];
 var imgloaded = 0;
+
+// Function to detect if the device is mobile
+function isMobileDevice() {
+  return window.innerWidth <= 768; // Adjust the breakpoint as needed
+}
+
+function getStartAnimation() {
+  if (isMobileDevice()) {
+    return function startAnimation() {
+      var updateframes = (index) => {
+        return {
+          currentIndex: index,
+          ease: "linear",
+          onUpdate: () => {
+            loadimage(Math.floor(frames.currentIndex));
+          },
+        };
+      };
+      var tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".page1",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 2,
+        },
+      });
+
+      tl.to(frames, updateframes(10), "a")
+        .to(
+          ".hometext",
+          {
+            opacity: 0,
+            ease: "linear",
+            delay: -0.4,
+          },
+          "a"
+        )
+        .to(
+          ".initialhomepage",
+          {
+            opacity: 0,
+            ease: "linear",
+          },
+          "a"
+        )
+        .to(frames, updateframes(120))
+        .to(frames, updateframes(120), "b")
+        .to(
+          ".whitediv",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "b"
+        )
+        .to(frames, updateframes(150), "d")
+        .to(
+          ".whitediv",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 0,
+          },
+          "d"
+        )
+        .to(frames, updateframes(235))
+        .to(frames, updateframes(235), "e")
+        .to(
+          ".whitediv1",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "e"
+        )
+        .to(frames, updateframes(260), "f")
+        .to(
+          ".whitediv1",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "f"
+        )
+        .to(frames, updateframes(450))
+        .to(frames, updateframes(450), "h")
+        .to(
+          ".whitediv2",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "h"
+        )
+        .to(frames, updateframes(470), "i")
+        .to(
+          ".whitediv2",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "i"
+        )
+        .to(frames, updateframes(555))
+        .to(frames, updateframes(555), "j")
+        .to(
+          ".whitediv3",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "j"
+        )
+        .to(frames, updateframes(585), "k")
+        .to(
+          ".whitediv3",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "k"
+        )
+        .to(frames, updateframes(710))
+        .to(frames, updateframes(710), "m")
+        .to(
+          ".whitediv4",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "m"
+        )
+        .to(frames, updateframes(740), "o")
+        .to(
+          ".whitediv4",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 0,
+          },
+          "o"
+        )
+        .to(frames, updateframes(1190))
+        .to(frames, updateframes(1190), "p")
+        .to(
+          ".whitediv5",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "p"
+        )
+        .to(frames, updateframes(1230), "r")
+        .to(
+          ".whitediv5",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 0,
+          },
+          "r"
+        )
+        .to(frames, updateframes(1500))
+        .to(frames, updateframes(1500), "s")
+        .to(
+          ".finalhomepage",
+          {
+            opacity: 1,
+            ease: "linear",
+          },
+          "s"
+        )
+        .to(
+          ".finalhometext",
+          {
+            opacity: 1,
+            ease: "linear",
+            delay: 0.4,
+          },
+          "s"
+        );
+    };
+  } else {
+    return function startAnimation() {
+      var updateframes = (index) => {
+        return {
+          currentIndex: index,
+          ease: "linear",
+          onUpdate: () => {
+            loadimage(Math.floor(frames.currentIndex));
+          },
+        };
+      };
+      var tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".page1",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 2,
+        },
+      });
+
+      tl.to(frames, updateframes(10), "a")
+        .to(
+          ".hometext",
+          {
+            opacity: 0,
+            ease: "linear",
+            delay: -0.4,
+          },
+          "a"
+        )
+        .to(
+          ".initialhomepage",
+          {
+            opacity: 0,
+            ease: "linear",
+          },
+          "a"
+        )
+        .to(frames, updateframes(100))
+        .to(frames, updateframes(100), "b")
+        .to(
+          ".whitediv",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "b"
+        )
+        .to(frames, updateframes(140), "c")
+        .to(
+          ".whitediv",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "c"
+        )
+        .to(frames, updateframes(180), "d")
+        .to(
+          ".whitediv",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 0,
+          },
+          "d"
+        )
+        .to(frames, updateframes(400))
+        .to(frames, updateframes(400), "e")
+        .to(
+          ".whitediv1",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "e"
+        )
+        .to(frames, updateframes(430), "f")
+        .to(
+          ".whitediv1",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "f"
+        )
+        .to(frames, updateframes(710))
+        .to(frames, updateframes(710), "h")
+        .to(
+          ".whitediv2",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "h"
+        )
+        .to(frames, updateframes(740), "i")
+        .to(
+          ".whitediv2",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "i"
+        )
+        .to(frames, updateframes(845))
+        .to(frames, updateframes(845), "j")
+        .to(
+          ".whitediv3",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "j"
+        )
+        .to(frames, updateframes(875), "k")
+        .to(
+          ".whitediv3",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "k"
+        )
+        .to(frames, updateframes(1040))
+        .to(frames, updateframes(1040), "m")
+        .to(
+          ".whitediv4",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "m"
+        )
+        .to(frames, updateframes(1070), "o")
+        .to(
+          ".whitediv4",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 0,
+          },
+          "o"
+        )
+        .to(frames, updateframes(1610))
+        .to(frames, updateframes(1610), "p")
+        .to(
+          ".whitediv5",
+          {
+            x: "0%",
+            ease: "linear",
+            opacity: 1,
+          },
+          "p"
+        )
+        .to(frames, updateframes(1630), "r")
+        .to(
+          ".whitediv5",
+          {
+            x: "-100%",
+            ease: "linear",
+            opacity: 0,
+          },
+          "r"
+        )
+        .to(frames, updateframes(1849))
+        .to(frames, updateframes(1879), "s")
+        .to(
+          ".finalhomepage",
+          {
+            opacity: 1,
+            ease: "linear",
+          },
+          "s"
+        )
+        .to(
+          ".finalhometext",
+          {
+            opacity: 1,
+            ease: "linear",
+            delay: 0.4,
+          },
+          "s"
+        );
+    };
+  }
+}
+
+const startsAnimation = getStartAnimation();
+var startingindex;
+// Function to preload images
 function preloadimages() {
-  for (var i = 1; i <= frames.maxIndex; i++) {
-    var imgUrl = `./Images/frame_${i.toString().padStart(4, "0")}.jpg`;
+  // Determine the folder and number of frames based on the device type
+  if (isMobileDevice()) {
+    startingindex = 230;
+    frames.maxIndex = 1746; // Number of frames for mobile
+    var folderPath = "./MobImages/";
+  } else {
+    startingindex = 1;
+    frames.maxIndex = 1879; // Number of frames for desktop
+    var folderPath = "./Images/";
+  }
+
+  for (var i = startingindex; i <= frames.maxIndex; i++) {
+    var imgUrl = `${folderPath}frame_${i.toString().padStart(4, "0")}.jpg`;
     var img = new Image();
     img.src = imgUrl;
+
     img.onload = () => {
       imgloaded++;
       if (imgloaded === frames.maxIndex) {
         loadimage(frames.currentIndex);
-        startAnimation();
+        startsAnimation();
+        loadinganimation();
       }
     };
+
     Images.push(img);
   }
 }
+
+// Call the preloadimages function
+preloadimages();
 
 function loadimage(index) {
   if (index >= 0 && index <= frames.maxIndex) {
@@ -45,245 +452,6 @@ function loadimage(index) {
     context.drawImage(imgs, offsetX, offsetY, newWidth, newHeight);
     frames.currentIndex = index;
   }
-}
-
-function startAnimation() {
-  var updateframes = (index) => {
-    return {
-      currentIndex: index,
-      ease: "linear",
-      onUpdate: () => {
-        loadimage(Math.floor(frames.currentIndex));
-      },
-    };
-  };
-  var tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".page1",
-      start: "top top",
-      end: "bottom bottom",
-      scrub: 2,
-    },
-  });
-
-  tl.to(frames, updateframes(30), "a")
-    .to(
-      ".hometext",
-      {
-        opacity: 0,
-        ease: "linear",
-        delay: -0.4,
-      },
-      "a"
-    )
-    .to(
-      ".initialhomepage",
-      {
-        opacity: 0,
-        ease: "linear",
-      },
-      "a"
-    )
-    .to(frames, updateframes(100), "b")
-    .to(
-      ".whitediv",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "b"
-    )
-    .to(frames, updateframes(140), "c")
-    .to(
-      ".whitediv",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "c"
-    )
-    .to(frames, updateframes(180), "d")
-    .to(
-      ".whitediv",
-      {
-        x: "-100%",
-        ease: "expo.out",
-        opacity: 0,
-      },
-      "d"
-    )
-    .to(frames, updateframes(380), "e")
-    .to(
-      ".whitediv1",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "e"
-    )
-    .to(frames, updateframes(440), "f")
-    .to(
-      ".whitediv1",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "f"
-    )
-    .to(frames, updateframes(680), "g")
-    .to(
-      ".whitediv1",
-      {
-        x: "-100%",
-        ease: "expo.out",
-        opacity: 0,
-      },
-      "g"
-    )
-    .to(frames, updateframes(710), "h")
-    .to(
-      ".whitediv2",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "h"
-    )
-    .to(frames, updateframes(800), "i")
-    .to(
-      ".whitediv2",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "i"
-    )
-    .to(frames, updateframes(810), "j")
-    .to(
-      ".whitediv2",
-      {
-        x: "-100%",
-        ease: "expo.out",
-        opacity: 0,
-      },
-      "j"
-    )
-    .to(frames, updateframes(880), "j")
-    .to(
-      ".whitediv3",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "j"
-    )
-    .to(frames, updateframes(990), "k")
-    .to(
-      ".whitediv3",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "k"
-    )
-    .to(frames, updateframes(1000), "l")
-    .to(
-      ".whitediv3",
-      {
-        x: "-100%",
-        ease: "expo.out",
-        opacity: 0,
-      },
-      "l"
-    )
-    .to(frames, updateframes(1020), "m")
-    .to(
-      ".whitediv4",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "m"
-    )
-    .to(frames, updateframes(1150), "n")
-    .to(
-      ".whitediv4",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "n"
-    )
-    .to(frames, updateframes(1200), "o")
-    .to(
-      ".whitediv4",
-      {
-        x: "-100%",
-        ease: "expo.out",
-        opacity: 0,
-      },
-      "o"
-    )
-    .to(frames, updateframes(1540))
-    .to(frames, updateframes(1570), "p")
-    .to(
-      ".whitediv5",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "p"
-    )
-    .to(frames, updateframes(1630), "q")
-    .to(
-      ".whitediv5",
-      {
-        x: "0%",
-        ease: "expo.out",
-        opacity: 1,
-      },
-      "q"
-    )
-    .to(frames, updateframes(1640), "r")
-    .to(
-      ".whitediv5",
-      {
-        x: "-100%",
-        ease: "expo.out",
-        opacity: 0,
-      },
-      "r"
-    )
-    .to(frames, updateframes(1849))
-    .to(frames, updateframes(1879), "s")
-    .to(
-      ".finalhomepage",
-      {
-        opacity: 1,
-        ease: "linear",
-      },
-      "s"
-    )
-    .to(
-      ".finalhometext",
-      {
-        opacity: 1,
-        ease: "linear",
-        delay: 0.4,
-      },
-      "s"
-    );
 }
 
 window.addEventListener("resize", () => {
@@ -325,8 +493,6 @@ const loadinganimation = () => {
       clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)",
     });
 };
-
-loadinganimation();
 
 var navbaranimation = () => {
   const navbar = document.querySelector("nav");
